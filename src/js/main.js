6c9c114c21a0734bff7440cf707c4b959eaad407
Jakub Hermyt <kubahermyt@gmail.com>
2019-08-25 22:07:00 +0200

    feat: hamburger menu animation

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,7 @@ const navButton = document.querySelector(`.navigation__button--js`);
 
 navButton.addEventListener(`click`, menu = () => {
     const hamburger = document.querySelector(`.navigation__list--js`);
-    hamburger.classList.toggle(`navigation__list--active`)
+    const icon = document.querySelector(`.navigation__button--js`);
+    hamburger.classList.toggle(`navigation__list--active`);
+    icon.classList.toggle(`hamburger--active`);
 })
