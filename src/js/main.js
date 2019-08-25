2ec636488c3487a7f9dc7a6690cf8247e59e3ba3
Jakub Hermyt <kubahermyt@gmail.com>
2019-08-25 19:25:44 +0200

    feat: hamburger menu fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
-
-
-
 console.log(`Hello world!`);
 
 
+const navButton = document.querySelector(`.navigation__button--js`);
+
+navButton.addEventListener(`click`, menu = () => {
+    const hamburger = document.querySelector(`.navigation__list--js`);
+    hamburger.classList.toggle(`navigation__list--active`)
+})
