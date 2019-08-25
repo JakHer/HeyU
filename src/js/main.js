7cae90a73d9530b679d9578f594de31742ec580a
Jakub Hermyt <kubahermyt@gmail.com>
2019-08-26 00:11:53 +0200

    feat: specificity problem solved, !important avoided :)

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,38 @@ const navButton = document.querySelector(`.navigation__button--js`);
 navButton.addEventListener(`click`, menu = () => {
     const hamburger = document.querySelector(`.navigation__list--js`);
     const icon = document.querySelector(`.navigation__button--js`);
-    hamburger.classList.toggle(`navigation__list--active`);
+    const timeout = 400;
     icon.classList.toggle(`hamburger--active`);
+    const addActive = () => {
+        hamburger.classList.add(`navigation__list--active`);
+    }
+    const removeActive = () => {
+        hamburger.classList.remove(`navigation__list--active`);
+    }
+
+    icon.classList.contains(`hamburger--active`) ?
+        addActive() :
+        removeActive();
+
+    const animIn = () => {
+        hamburger.classList.add(`slideIn`);
+        setTimeout(() => {
+            hamburger.classList.remove(`slideIn`)
+        }, timeout);
+    }
+
+
+    const animOut = () => {
+        hamburger.classList.add(`navigation__list--active`)
+        hamburger.classList.add(`slideOut`);
+        setTimeout(() => {
+            hamburger.classList.remove(`slideOut`);
+            hamburger.classList.remove(`navigation__list--active`)
+        }, timeout);
+
+    }
+
+    hamburger.classList.contains(`navigation__list--active`) ?
+        animIn() :
+        animOut()
 })
